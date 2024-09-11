664f0c6860dc6eb12163594772a2043bd7055334
Mohammed Yunus Shaikh <shaikhyunus.us89@gmail.com>
2024-09-11 21:48:46 +0530

    Data Ingestion Added

---
+++ src/cnnClassifier/config/__init__.py
@@ -0,0 +1,5 @@
+# File: /Volumes/Macintosh HD/Users/mohammedyunusshaikh/Github Projects/Kidney-Disease-Classification-Deep-Learning-Project/src/cnnClassifier/config/__init__.py
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
